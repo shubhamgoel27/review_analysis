ea3cc9fcb83c074bd995ec216606d5b5a047d941
shubhamgoel27 <shubhamgoel27@gmail.com>
2015-07-16 16:32:17 +0530

    true ya false

--- tagger.py
+++ tagger.py
@@ -76,7 +76,7 @@ class Tagger(object):
         for phone in self.data:
             phone['noise'] = []
             for i, review in enumerate(phone['reviews']):
-                found = True
+                found = False
                 for word in self.tag_list:
                     if word in review[3] or word in review[1]:
                         found = True
